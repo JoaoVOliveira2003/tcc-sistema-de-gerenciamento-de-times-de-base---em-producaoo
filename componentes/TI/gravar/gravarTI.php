95cca6e19b7ba595a5ae4bb6b05748b648e0a39e
JoaoVOliveira2003 <ojoao953@gmail.com>
2025-05-21 22:03:45 -0300

    .

--- componentes/TI/gravar/gravarTI.php
+++ componentes/TI/gravar/gravarTI.php
@@ -12,14 +12,15 @@ $cpf = getPost('cpf');
 $cpf = preg_replace('/[^0-9]/', '', $cpf);
 $cod_role = 1;
 
+$emailBase = 'ojoao953@gmail.com';
+
 $bd = conecta();
 
 
 $query = "SELECT COUNT(*) FROM login_usuario WHERE email_usuario = '$email'";
-
 if ($bd->SqlExecuteQuery($query)) {
     $count = $bd->SqlQueryShow("COUNT(*)");
-    if ($count > 0) {
+    if ($count > 0 && $email == $emailBase) {
         $retorno = 'emailJaCadastrado';
         $bd->SqlDisconnect();
         exit($retorno);
@@ -37,12 +38,21 @@ if ($bd->SqlExecuteQuery($query)) {
                values ($cod_pessoa, $cod_role)";
 
     if ($bd->SqlExecuteQuery($query)) {
-        enviarGmail($email, $nome, $cod_role, $cod_pessoa);
-        $retorno = 'ok';
+
+        $query = "INSERT INTO login_usuario (email_usuario,cod_usuario) VALUES ('$email', $cod_pessoa,)";
+
+        if ($bd->SqlExecuteQuery($query)) {
+            enviarGmail($email, $nome, $cod_role, $cod_pessoa);
+            $retorno = 'ok';
+        }
     } else {
         $retorno = 'nok';
     }
+
+} else {
+    $retorno = 'nok';
 }
+
 
 
 $bd->SqlDisconnect();
