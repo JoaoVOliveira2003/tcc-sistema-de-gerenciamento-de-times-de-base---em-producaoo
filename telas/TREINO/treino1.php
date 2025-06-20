3711fb44e93680ce6dd748e5585e019c669c1be3
JoaoVOliveira2003 <ojoao953@gmail.com>
2025-06-20 20:37:21 -0300

    .

---
+++ telas/TREINO/treino1.php
@@ -0,0 +1,6 @@
+<!-- tela1.php -->
+<form method="POST" action="treino2.php">
+  <input type="text" name="tempo" placeholder="Tempo">
+  <input type="text" name="pessoas" placeholder="Pessoas">
+  <button type="submit">Enviar</button>
+</form>
---
+++ telas/TREINO/treino2.php
@@ -0,0 +1,7 @@
+<?php
+$tempo = $_POST['tempo'];
+$pessoas = $_POST['pessoas'];
+
+echo "Tempo: $tempo, Pessoas: $pessoas.";
+?>
+
